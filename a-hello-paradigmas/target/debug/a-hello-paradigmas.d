6a9acb89f7ba1712cf43b6d0da87634276176f67
Celso Crivelaro <celsocrivelaro@gmail.com>
2020-08-29 11:26:44 -0300

    Hello, world

---
+++ a-hello-paradigmas/target/debug/a-hello-paradigmas.d
@@ -0,0 +1,1 @@
+/Users/crivelaro/repos/senac/paradigmas/sistema-rust/a-hello-paradigmas/target/debug/a-hello-paradigmas: /Users/crivelaro/repos/senac/paradigmas/sistema-rust/a-hello-paradigmas/src/main.rs
---
+++ a-hello-paradigmas/target/debug/deps/a_hello_paradigmas.d
@@ -0,0 +1,5 @@
+/Users/crivelaro/repos/senac/paradigmas/sistema-rust/a-hello-paradigmas/target/debug/deps/a_hello_paradigmas: src/main.rs
+
+/Users/crivelaro/repos/senac/paradigmas/sistema-rust/a-hello-paradigmas/target/debug/deps/a_hello_paradigmas.d: src/main.rs
+
+src/main.rs:
