a27c03117898f98463d8bffe3fcd100f12cec371
Celso Crivelaro <celsocrivelaro@gmail.com>
2020-08-31 13:11:18 -0300

    Usando biblioteca

--- f-linha-comando/src/main.rs
+++ f-linha-comando/src/main.rs
@@ -1,18 +1,22 @@
+use structopt::StructOpt;
 
 // 1 - Definindo uma estrutura da linha de comando
+#[derive(StructOpt)]
 struct Cli{
   padrao: String,
+  #[structopt(parse(from_os_str))]
   arquivo: std::path::PathBuf,
 }
 
 fn main() {
   // 2 - Padrões de entrada
-  let padrao = std::env::args().nth(1).expect("no pattern given");
-  let caminho = std::env::args().nth(2).expect("no path given");
-  let args = Cli {
-    padrao: padrao,
-    arquivo: std::path::PathBuf::from(caminho),
-  };
+  //let padrao = std::env::args().nth(1).expect("no pattern given");
+  //let caminho = std::env::args().nth(2).expect("no path given");
+  // let args = Cli {
+  //   padrao: padrao,
+  //   arquivo: std::path::PathBuf::from(caminho),
+  // };
+  let args = Cli::from_args();
 
   let content = std::fs::read_to_string(&args.arquivo)
     .expect("could not read file");
